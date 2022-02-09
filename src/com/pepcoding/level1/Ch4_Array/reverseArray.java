08927659b50c924c4d00801648a467386b3ec4a7
callmepankaj <pankmaurya23@gmail.com>
2022-02-09 22:59:13 +0530

    Java

---
+++ src/com/pepcoding/level1/Ch4_Array/reverseArray.java
@@ -0,0 +1,23 @@
+package com.pepcoding.level1.Ch4_Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class reverseArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
